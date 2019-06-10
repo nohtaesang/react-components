f33f005e130b08f536bf204dc0620437dadab94c
nohtaesang <nohtaesang@naver.com>
2019-06-10 10:04:11 +0900

    modal

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PaginationExample from './js/paginationExample';
 import ToastifyExample from './js/toastifyExample';
+import ModalExample from './js/modalExample';
 const App = () => {
 	return (
 		<div>
 			<PaginationExample />
 			<ToastifyExample />
+			<ModalExample />
 		</div>
 	);
 };
--- src/js/modalExample.js
+++ src/js/modalExample.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const ModalExample = () => {};
+const ModalExample = () => {
+	const onClickModalActive = () => {
+		console.log('here');
+	};
+
+	return (
+		<div>
+			<button onClick={onClickModalActive}>Modal Active</button>
+		</div>
+	);
+};
 
 export default ModalExample;
